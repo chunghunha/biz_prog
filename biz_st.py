8d1b4b15be5b2f788d7eb4870ce9015b97c23f7f
Chunghun Ha <chunghun.ha@gmail.com>
2026-05-26 16:21:02 +0900

    Revise Streamlit app for business model analysis
    Updated the content and structure of the Streamlit app to include business model analysis, various text formatting, and media elements.

--- biz_st.py
+++ biz_st.py
@@ -1,22 +1,42 @@
 import streamlit as st  # streamlit 라이브러리 임포트
 
-# 타이틀 텍스트 출력
-st.title('첫번째 웹 어플 만들기 👋📺🚋🤶🏾')
+"""
+# 비즈니스 모델 분석 📚
 
-"# 첫번째 웹 어플 만들기 👋📺🚋🤶🏾"
-"###### 첫번째 웹 어플 만들기 👋📺🚋🤶🏾"
+[네이버](https://www.naver.com)  
+[홍익대학교](https://www.hongik.ac.kr)
 
-'''### 👑 Magic 적용
-1. ordered item
-    - 강조: **unordered item**
-    - 기울임: *unordered item*
-1. ordered item
-1. ordered item
-1. ordered item
-'''
+이것이 일반 본문 **이것이 굵은 글씨** *이것이 기울임 글씨* ~~이것이 취소선~~
 
-'''
-- unordered item
-    - unordered item
-        - unordered item
-'''
+:red[빨간색 글씨] :green[초록색 글씨] :blue[파란색 글씨]
+
+```python
+import streamlit as st
+
+print("코드 블록")
+```
+"""
+st.caption('캡션(작고 흐린 글씨로 표현됨) : st.caption()')
+
+with st.echo():
+    # 이 블록의 코드와 결과를 출력
+    name = 'Chunghun Ha'
+    st.write("Hello, Streamlit!", name)
+
+st.latex('\int_a^b f(x)dx')
+"$$\int_a^b f(x)dx$$"
+
+'#### :orange[이미지: st.image()]'
+st.image("./data/python설명.jpeg", caption="파이썬 로고", width=300)
+
+'#### :orange[오디오: st.audio()]'
+st.audio("./data/bombinsound.mp3", format="audio/mpeg", loop=True)
+
+'#### :orange[동영상: st.video()]'
+# 'rb' : 바이너리 모드로 파일 열기
+video_file = open("./data/성당.mp4", "rb")
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+st.divider()  # 👈 구분선
