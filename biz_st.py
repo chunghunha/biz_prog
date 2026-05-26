a9b34197e58057f2aae07a5c08a00aeb8f5b401f
Chunghun Ha <chunghun.ha@gmail.com>
2026-05-26 15:16:28 +0900

    Add files via upload

--- biz_st.py
+++ biz_st.py
@@ -3,6 +3,20 @@ import streamlit as st  # streamlit 라이브러리 임포트
 # 타이틀 텍스트 출력
 st.title('첫번째 웹 어플 만들기 👋📺🚋🤶🏾')
 
-"## 이건 부제목"
+"# 첫번째 웹 어플 만들기 👋📺🚋🤶🏾"
+"###### 첫번째 웹 어플 만들기 👋📺🚋🤶🏾"
 
-"- 이건 목록"
+'''### 👑 Magic 적용
+1. ordered item
+    - 강조: **unordered item**
+    - 기울임: *unordered item*
+1. ordered item
+1. ordered item
+1. ordered item
+'''
+
+'''
+- unordered item
+    - unordered item
+        - unordered item
+'''
